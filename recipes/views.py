d775b55b2c55a55e5736e8c70a4e4bd9a91cf707
RuanGemmer <ruangemmer@live.com>
2023-08-18 16:36:11 -0300

    Configurated views to get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.utils.translation import gettext as _
 from django.utils import translation
-from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 html_language = translation.get_language()
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(is_published=True).order_by('-id')
+    )
+
     return render(request, "recipes/pages/home.html", context={
         'html_language': html_language,
         'title': 'Home',
@@ -17,25 +19,29 @@ def home(request):
 
 
 def category(request, id):
-    recipes = Recipe.objects.filter(
-        category__id=id, is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=id, is_published=True
+        ).order_by('-id')
+    )
+    title = _('Category')
+
     return render(request, "recipes/pages/home.html", context={
         'html_language': html_language,
-        'title': 'Home',
+        'title': f'{title} - {recipes[0].category.name}',
         'recipes': recipes,
     })
 
 
 def recipes(request, id: int):
-    recipe = Recipe.objects.filter(
-        id=id
-    ).first()
+    recipe = get_object_or_404(
+        Recipe.objects.filter(id=id, is_published=True)
+    )
     title: str = _('Recipe')
 
     return render(request, "recipes/pages/recipes-view.html", context={
         'html_language': html_language,
-        'title': f'{title} {id}',
+        'title': f'{title}: {recipe.title}',
         'recipe': recipe,
         'is_detail_page': True,
     })
