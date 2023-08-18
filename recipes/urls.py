974433e3374c03687139a8365ea7f93eef261725
RuanGemmer <ruangemmer@live.com>
2023-08-18 15:51:03 -0300

    Configurated models, django admini page and views from database

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('recipes/<int:id>/', views.recipes, name='recipe'),
+    path('recipes/category/<int:id>/', views.category, name='category'),
     path('', views.home, name='home'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,24 +2,40 @@ from django.shortcuts import render
 from django.utils.translation import gettext as _
 from django.utils import translation
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 html_language = translation.get_language()
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, "recipes/pages/home.html", context={
         'html_language': html_language,
         'title': 'Home',
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, id):
+    recipes = Recipe.objects.filter(
+        category__id=id, is_published=True
+    ).order_by('-id')
+    return render(request, "recipes/pages/home.html", context={
+        'html_language': html_language,
+        'title': 'Home',
+        'recipes': recipes,
     })
 
 
 def recipes(request, id: int):
+    recipe = Recipe.objects.filter(
+        id=id
+    ).first()
     title: str = _('Recipe')
 
     return render(request, "recipes/pages/recipes-view.html", context={
         'html_language': html_language,
         'title': f'{title} {id}',
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
